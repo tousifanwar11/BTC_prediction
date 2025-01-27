6774bc171d7f6531d5bffb2e74bdaf131b19ce61
Tousif Anwar <tanwar@wgu.edu>
2025-01-27 01:18:00 +0000

    dwonloaded btc historical data

---
+++ get_btc_price_data.py
@@ -0,0 +1,13 @@
+import yfinance as yf
+import pandas as pd
+
+# Download Bitcoin Price data for the last 10 years
+btc_data = yf.download("BTC-USD", period = "10y")
+
+# Save the data to a csv file
+btc_data.to_csv('btc_price.csv')
+
+print("BTC price data downloaded and saved to btc_price.csv")
+
+
+
